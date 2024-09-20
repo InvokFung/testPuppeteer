c978507603409e7c27f50d62608b5e61631f6512
FUNG Kwan Tai <44592591+InvokFung@users.noreply.github.com>
2024-09-20 14:10:01 +0800

    Testing

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,9 @@ const cors = require("cors");
 const app = express();
 const port = 8081;
 const { Worker } = require("worker_threads");
+const { generatePdf } = require("./public/worker");
+const path = require("path");
+const fs = require("fs");
 
 app.use(express.static("public"));
 app.use(express.json());
@@ -20,19 +23,58 @@ app.get("/test", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
 });
 
-app.post("/calculate", (req, res) => {
+app.get("/check", async (req, res) => {
+  try {
+    const pdfBuffer = await generatePdf();
+
+    res.writeHead(200, {
+      "Content-Type": "application/pdf",
+      "Content-Disposition": "attachment; filename=test.pdf",
+      "Content-Length": pdfBuffer.length,
+    });
+    res.end(pdfBuffer);
+    // fs.writeFile(filePath, pdfBuffer, (err) => {
+    //   if (err) {
+    //     console.error("Error writing PDF to file:", err);
+    //     res.status(500).send("Error generating PDF");
+    //   } else {
+    //     res.send("PDF generated and saved as example.pdf");
+    //   }
+    // });
+  } catch (error) {
+    console.error("Error generating PDF:", error);
+    res.status(500).send("Error generating PDF");
+  }
+});
+
+app.post("/calculate", async (req, res) => {
   const { number } = req.body;
-  const worker = new Worker(__dirname + "/public/worker.js");
-  worker.on("message", (result) => {
-    res.json({ result });
+  const test = await generatePdf();
+  res.writeHead(200, {
+    "Content-Type": "application/pdf",
+    "Content-Disposition": "attachment; filename=test.pdf",
+    "Content-Length": test.length,
   });
-  worker.on("error", (msg) => {
-    console.log(msg);
-  });
-  worker.on("exit", () => {
-    console.log("worker is exited");
-  });
-  worker.postMessage(number);
+  res.end(test);
+
+  // const worker = new Worker(__dirname + "/public/worker.js");
+  // worker.on("message", (result) => {
+  //   // res.json({ result });
+  //   if (result) {
+  //     res.setHeader("Content-Type", "application/pdf");
+  //     res.setHeader("Content-Disposition", 'inline; filename="output.pdf"');
+  //     res.send(result);
+  //   } else {
+  //     res.status(500).send("Error generating PDF");
+  //   }
+  // });
+  // worker.on("error", (msg) => {
+  //   console.log(msg);
+  // });
+  // worker.on("exit", () => {
+  //   console.log("worker is exited");
+  // });
+  // worker.postMessage(number);
 });
 
 app.listen(port, () => {
--- backend/public/worker.js
+++ backend/public/worker.js
@@ -1,6 +1,95 @@
-const { parentPort } = require("worker_threads");
+// const { parentPort } = require("worker_threads");
+const puppeteer = require("puppeteer");
 
-parentPort.on("message", (msg) => {
-  let result = 2 * msg;
-  parentPort.postMessage(result);
-});
+async function generatePdf() {
+  const browser = await puppeteer.launch({
+    headless: "new",
+    args: [
+      "--disable-features=IsolateOrigins",
+      "--disable-site-isolation-trials",
+      "--autoplay-policy=user-gesture-required",
+      "--disable-background-networking",
+      "--disable-background-timer-throttling",
+      "--disable-backgrounding-occluded-windows",
+      "--disable-breakpad",
+      "--disable-client-side-phishing-detection",
+      "--disable-component-update",
+      "--disable-default-apps",
+      "--disable-dev-shm-usage",
+      "--disable-domain-reliability",
+      "--disable-extensions",
+      "--disable-features=AudioServiceOutOfProcess",
+      "--disable-hang-monitor",
+      "--disable-ipc-flooding-protection",
+      "--disable-notifications",
+      "--disable-offer-store-unmasked-wallet-cards",
+      "--disable-popup-blocking",
+      "--disable-print-preview",
+      "--disable-prompt-on-repost",
+      "--disable-renderer-backgrounding",
+      "--disable-setuid-sandbox",
+      "--disable-speech-api",
+      "--disable-sync",
+      "--hide-scrollbars",
+      "--ignore-gpu-blacklist",
+      "--metrics-recording-only",
+      "--mute-audio",
+      "--no-default-browser-check",
+      "--no-first-run",
+      "--no-pings",
+      "--no-sandbox",
+      "--no-zygote",
+      "--password-store=basic",
+      "--use-gl=swiftshader",
+      "--use-mock-keychain",
+    ],
+  });
+  const page = await browser.newPage();
+
+  await page.emulateMediaType("print");
+  await page.setViewport({
+    width: 1600,
+    height: 900,
+    isLandscape: true,
+  });
+
+  const pdfContent = `
+    <html>
+      <body>
+        <div>
+          <h1>Hello, Testing!</h1>
+        </div>
+      </body>
+    </html>
+  `;
+  await page.setContent(pdfContent);
+
+  await page.setContent(pdfContent), { waitUntil: "networkidle0" };
+
+  // Generate PDF buffer
+  const pdfBuffer = await page.pdf({
+    path: "example.pdf",
+    margin: { top: 0, right: 0, bottom: 0, left: 0 },
+    width: "1600px",
+    height: "900px",
+    preferCSSPageSize: true,
+    printBackground: true,
+  });
+
+  await browser.close();
+  return pdfBuffer;
+}
+
+// parentPort.on("message", async (msg) => {
+//   let result = 2 * msg;
+//   try {
+//     const pdfBuffer = await generatePdf();
+//     // console.log(pdfBuffer);
+//     parentPort.postMessage(pdfBuffer);
+//   } catch (e) {
+//     console.log(e);
+//     parentPort.postMessage(null);
+//   }
+// });
+
+module.exports = { generatePdf };
